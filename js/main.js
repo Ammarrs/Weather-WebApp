d64059b4f28306f991f2b94fb34134cf4e06826d
Ammarrs <ltop62060@gmail.com>
2025-05-23 12:08:30 +0300

    update: weather status changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const apiKey = "9a48024b30abec2df019e1522c5fa6e8";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather_icon");
 
 async function checkWeather (city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -13,6 +14,20 @@ async function checkWeather (city) {
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "./weather-app-img/images/clouds.png";
+  } else if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "./weather-app-img/images/clear.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "./weather-app-img/images/rain.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "./weather-app-img/images/drizzle.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "./weather-app-img/images/mist.png";
+  } else if (data.weather[0].main === "Snow") {
+    weatherIcon.src = "./weather-app-img/images/snow.png";
+  }
 }
 
 searchBtn.addEventListener("click", () => {
